740480f13bff8220b4a04f6a964d5fe7bd363bb2
ryanwren <rwren@cloudbees.com>
2018-09-13 23:59:45 -0400

    ANALYTICS-2670 Breaking

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -8,7 +8,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "This is the Loki App!!" );
+		System.out.println( "This is the Loki App!!" );
+		Break!!
     }
 }
 
