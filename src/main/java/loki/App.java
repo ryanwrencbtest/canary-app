3c9ca1708f7390f56e218b135efcb6909dfc8e8d
ryanwren <rwren@cloudbees.com>
2018-09-13 23:55:30 -0400

    Revert "ANALYTICS-2672 Break on purpose."
    This reverts commit ac1e079779ecaf2d8f2a35b00d514e1b7df06317.

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -8,8 +8,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		System.out.println( "This is the Loki App!!" );
-		breaking on purpose
+	System.out.println( "This is the Loki App!!" );
     }
 }
 
